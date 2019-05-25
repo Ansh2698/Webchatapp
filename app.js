1f9a4ddd84a00a2101fdea5d18589914771e2ddd
Ansh <ansh.agrawal.che17@itbhu.ac.in>
2019-05-25 14:45:41 +0530

    Using Passport Module

--- app.js
+++ app.js
@@ -3,8 +3,17 @@ var bodyParser=require("body-parser");
 var ejs=require("ejs");
 var http=require("http");
 var container=require("./container");
+var flash=require("connect-flash");
+var cookieParser=require("cookie-parser");
+var expressValidator=require("express-validator");
+var session=require("express-session");
+var MongoStore=require("connect-mongo")(session);
+var mongoose=require("mongoose");
+var passport=require("passport");
 
 container.resolve(function(users){
+    mongoose.connect('mongodb://localhost/Webchatapp',{ useNewUrlParser: true });
+    mongoose.Promise = global.Promise;
     var app=SetupExpress();
     function SetupExpress(){
         var app=express();
@@ -20,8 +29,18 @@ container.resolve(function(users){
 
     function ConfigureExpress(app){
         app.use(express.static("public"));
+        app.use(cookieParser());
         app.set("view engine","ejs");
         app.use(bodyParser.json());
-        app.use(bodyParser.urlencoded({ extended: true }))
+        app.use(bodyParser.urlencoded({ extended: true }));
+        app.use(expressValidator());
+        app.use(session({
+            secret: 'keyboard cat',
+            resave: true,
+            saveUninitialized: true,
+            store:new MongoStore({mongooseConnection:mongoose.connection})
+          }))
+        app.use(passport.initialize());
+        app.use(passport.session());
     }
 })
--- controllers/users.js
+++ controllers/users.js
@@ -3,9 +3,13 @@ module.exports = function (_) {
     return {
         SetRouting: function (router) {
             router.get("/", this.indexPage);
+            router.get("/signup",this.getSignup);
         },
         indexPage: function (req, res) {
             return res.render("index", { test:"This is test message" });
+        },
+        getSignup:function(req,res){
+            return res.render("signup",{signup:"this is a signup page"});
         }
     }
 }
