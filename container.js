f5d14521fbd86f2926edfcf1d882911397a2f97b
Ansh <ansh.agrawal.che17@itbhu.ac.in>
2019-05-25 13:34:45 +0530

    Intial Modification

---
+++ container.js
@@ -0,0 +1,20 @@
+var dependable=require("dependable");
+var path=require("path");
+var container=dependable.container();
+
+var MyModule=[
+    ["_","lodash"]
+];
+MyModule.forEach(function(val){
+    container.register(val[0],function(){
+        return require(val[1]);
+    })
+});
+container.load(path.join(__dirname,"/controllers"));
+container.load(path.join(__dirname,"/helpers"));
+
+
+container.register("container",function(){
+    return container;
+});
+module.exports=container;
